995f202c70fa6aaa3153934176c0b7cdad7d2b9b
Wilfried <wilfried.djopa@gmail.com>
2019-11-26 15:25:34 +0100

    Mise à jour des entreprises

--- index.js
+++ index.js
@@ -6,34 +6,35 @@ var _ = require('lodash');
 var yahooFinance = require('yahoo-finance');
 
 var SYMBOLS = [
-    'KORI',
-  'AAPL',
-  'AMZN',
-  'GOOGL',
-  'YHOO'
+    'KORI.PA',
+    'OR.PA',
+    'FTI.PA',
+    'ATO.PA',
+    'BIM.PA',
+    'SOP.PA'
 ];
 
 yahooFinance.historical({
-  symbols: SYMBOLS,
-  from: '2019-01-01',
-  to: '2019-10-31',
-//   period: 'd'
+    symbols: SYMBOLS,
+    from: '2019-11-18',
+    to: '2019-11-24',
+    period: 'd'
 }, function (err, result) {
-  if (err) { throw err; }
-  _.each(result, function (quotes, symbol) {
-    console.log(util.format(
-      '=== %s (%d) ===',
-      symbol,
-      quotes.length
-    ).cyan);
-    if (quotes[0]) {
-      console.log(
-        '%s\n...\n%s',
-        JSON.stringify(quotes[0], null, 2),
-        JSON.stringify(quotes[quotes.length - 1], null, 2)
-      );
-    } else {
-      console.log('N/A');
-    }
-  });
+    if (err) { throw err; }
+    _.each(result, function (quotes, symbol) {
+        console.log(util.format(
+            '=== %s (%d) ===',
+            symbol,
+            quotes.length
+        ).cyan);
+        if (quotes[0]) {
+            console.log(
+                '%s\n...\n%s',
+                JSON.stringify(quotes[0], null, 2),
+                JSON.stringify(quotes[quotes.length - 1], null, 2)
+            );
+        } else {
+            console.log('N/A');
+        }
+    });
 });
